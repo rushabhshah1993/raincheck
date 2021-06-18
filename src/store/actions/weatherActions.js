440bc704d2612f8865ce9aee8a80d9e7178d2936
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2021-06-18 22:22:48 +0530

    New build

--- src/store/actions/weatherActions.js
+++ src/store/actions/weatherActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchCityWeather = (city = 'Mumbai') => {
     return dispatch => {
-        let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=60dfad51347e098c9a6b000ced44c353`;
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=60dfad51347e098c9a6b000ced44c353`;
         axios.get(url)
             .then(response => {
                 dispatch(setWeatherData(response.data.city, response.data.list));
